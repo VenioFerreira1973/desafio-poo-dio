ad67454ac2f344e5f67c89c72e4a89ec03ef8b96
VenioFerreira1973 <venioferreira@gmail.com>
2023-05-03 17:24:46 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +20,39 @@ public class Main {
         mentoria.setDescricao("descrição mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcampo Java developer");
+        bootcamp.setDescricao("Descricão Bootcamp Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Venio");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Aluno: " + dev1.getNome());
+        System.out.println("Conteudos inscritos: " + dev1.getConteudosIncritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos inscritos: " + dev1.getConteudosIncritos());
+        System.out.println("Conteudos inscritos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXP());
+
+        System.out.println("______________________________________________________________");
+        Dev dev2 = new Dev();
+        dev2.setNome("João");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Aluno: " + dev2.getNome());
+        System.out.println("Conteudos inscritos: " + dev2.getConteudosIncritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Conteudos inscritos: " + dev2.getConteudosIncritos());
+        System.out.println("Conteudos inscritos: " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularTotalXP());
+
 
 
 
